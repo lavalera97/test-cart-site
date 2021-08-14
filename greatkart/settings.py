6c655bdf21acc12f68c6201ddcec360f5def8b29
Valery <fenixor0007@gmail.com>
2021-08-14 17:04:55 +0300

    eb updaate

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['greatkart-django-env.eba-cuzzrc3e.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
